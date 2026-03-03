24279b429739739933b9ad3a88451a9f37e2e275
lstvedith-max <lstvedith@gmail.com>
2026-03-03 15:20:59 +0800

    login test script

--- cypress/e2e/project-edith-promax.cy.js
+++ cypress/e2e/project-edith-promax.cy.js
@@ -1,5 +1,15 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('/')
+    cy.get('[name="gl_comcde"]').click().clear().type('QATEAM') //Company Code
+    cy.get('[name="txtusrcde"]').click().clear().type('EDITH') //Username
+    cy.get('[name="txtusrpwd"]').click().clear().type('Ayi0429@') //Password
+    cy.get('.loginbtn > input').click() //Login button
+    cy.wait(2000) // Wait for 2 Seconds
+    cy.get('.ajs-content').should('contain', 'Successfully logged in') //Verify login success message
+    cy.get('.ajs-button').click() //Close the success message
+    cy.url().should('include', '/login_applist.php') //Varification of URL after login
+    
+    
   })
 })
